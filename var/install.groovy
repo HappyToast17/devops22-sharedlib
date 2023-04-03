57e210cb2b335588a9b086a4331914867924ec06
HappyToast17 <114514168+HappyToast17@users.noreply.github.com>
2023-04-03 21:18:21 +0300

    Delete var directory

--- var/install.groovy
+++
@@ -1,7 +0,0 @@
-def call() {
-                sh "apt-get update && apt-get install -y python3"
-                sh "apt-get install -y python3-pip"
-                sh "pip3 install pytest"
-                sh "pip3 install pylint"
-                sh "pip3 install -r requirements.txt"
-           }
